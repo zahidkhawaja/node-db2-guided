5de4d6dca11381b744c89555befa025d3ddc2571
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-17 12:01:00 +0100

    wip

---
+++ data/migrations/20200317115803_addColorColToFruitsTbl.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+  return knex.schema.table('fruits', table => {
+    table.string('color', 128)
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.table('fruits', table => {
+    table.dropColumn()
+  })
+};
